add72f3cb96cbe4ed096b31382c6984feb719431
lucymahony <lucymahony7@gmail.com>
2023-09-05 16:52:42 +0100

    changed star to an earlier verion

--- STAR/mapping/star_mapping_reads.sh
+++ STAR/mapping/star_mapping_reads.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source /ei/software/staging/RCSUPPORT-1153/stagingloader #STAR
-
+source package /tgac/software/testing/bin/STAR-2.3.0 # using version 2.3.0 as later versions seem to have bugs
 mapfile -t TISSUES < tissue.list
 tissue=${TISSUES[$SLURM_PROCID]}
 
@@ -9,11 +8,11 @@ read_1=../../intermediate_data/trimmed_reads/read_1/trimmed_${tissue}_1P.fastq.g
 read_2=../../intermediate_data/trimmed_reads/read_2/trimmed_${tissue}_2P.fastq.gz
 
 THREADS=16
-GENOME_DIR=../../intermediate_data/genome_directory
-OUT_DIR=../../intermediate_data/mapped_reads
+GENOME_DIR=../../intermediate_data/genome_directory/
+OUT_DIR=../../intermediate_data/mapped_reads/
 
 
-STAR --runThreadN $THREADS --genomeDir $GENOME_DIR --readFilesIn $read_1 $read_2 --readFilesCommand zcat --outFileNamePrefix $OUT_DIR/$tissue --outSAMtype BAM SortedByCoordinate --limitGenomeGenerateRAM 1001110470922 --limitBAMsortRAM 3000000000
+STAR --runThreadN $THREADS --genomeDir $GENOME_DIR --readFilesIn $read_1 $read_2 --readFilesCommand zcat --outFileNamePrefix $OUT_DIR
 
 echo "Done mapping for $tissue"
 
