f7224f1fa8a2d885ead74fd3ee05316cea9f1a82
lucymahony <lucymahony7@gmail.com>
2023-09-05 17:05:29 +0100

    post map processing scripts added

---
+++ STAR/post_mapping_analysis/list_parrallel_tasks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+declare -a tissues=("IS1" "IS2" "IS3" "SP1" "SP2" "SP3" "RO1" "RO2" "RO3" "LE1" "LE2" "LE3") 
+declare -a suffixes=("Aligned.sortedByCoord.out.bam" "Aligned.sortedByCoord.out_mapq20.bam")
+declare -a prefixes=("UO" "UM")
+
+> tissue.list
+for tissue in "${tissues[@]}"; do
+    for index in "${!suffixes[@]}"; do
+        echo "${tissue} ${suffixes[$index]} ${prefixes[$index]}" >> tissue.list
+   done
+done
+
+
