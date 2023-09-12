712a234f0a946d1f948ef53fd69aceb623a1e12b
lucymahony <lucymahony7@gmail.com>
2023-09-12 15:44:32 +0100

    changed input sam file names

--- STAR/post_mapping_analysis/list_parrallel_tasks.sh
+++ STAR/post_mapping_analysis/list_parrallel_tasks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
 declare -a tissues=("IS1" "IS2" "IS3" "SP1" "SP2" "SP3" "RO1" "RO2" "RO3" "LE1" "LE2" "LE3") 
-declare -a suffixes=("uniquelymapped_${tissue}.sam" "${tissue}.out.sam")
+declare -a input_sam_name=("uniquelymapped_${tissue}.sam" "${tissue}.out.sam")
 declare -a prefixes=("UO" "UM")
 
 > tissue.list
 for tissue in "${tissues[@]}"; do
-    for index in "${!suffixes[@]}"; do
-        echo "${tissue} ${suffixes[$index]} ${prefixes[$index]}" >> tissue.list
+    for index in "${!input_sam_name[@]}"; do
+        echo "${tissue} ${input_sam_name[$index]} ${prefixes[$index]}" >> tissue.list
    done
 done
 
