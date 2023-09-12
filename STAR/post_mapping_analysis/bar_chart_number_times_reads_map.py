fa2438b70c1f6638d2874c0552a67d3840d220e8
lucymahony <lucymahony7@gmail.com>
2023-09-12 11:25:18 +0100

    Plot gird of bar charts

--- STAR/post_mapping_analysis/bar_chart_number_times_reads_map.py
+++ STAR/post_mapping_analysis/bar_chart_number_times_reads_map.py
@@ -12,4 +12,31 @@ number_reads_mapping = pd.read_csv("number_reads_mapping.csv", sep=",", header=0
 # Make a new column for tissue type which is the first two characters of the column tissue
 number_reads_mapping['tissue_type'] = number_reads_mapping['tissue'].str[:2]
 
+# Inspect the data
 print(number_reads_mapping.head())
+
+# Calculate the mean value for each number_of_hits_in_reference within each tissue_type
+mean_number_reads_mapping = number_reads_mapping.groupby(['tissue_type', 'number_of_hits_in_reference'])['number_of_reads'].mean().reset_index()
+
+# Find the unique tissue_types
+tissue_types = number_reads_mapping['tissue_type'].unique()
+
+# Create a 2x2 grid of bar charts
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+axes = axes.flatten()
+
+for i, tissue_type in enumerate(tissue_types):
+    ax = axes[i]
+
+    # Plot the bar chart using the mean values
+    sns.barplot(x='number_of_hits_in_reference', y='number_of_reads',
+                data=mean_number_reads_mapping[mean_number_reads_mapping['tissue_type'] == tissue_type], ax=ax, ci=None)
+
+    # Overlay the individual data points as gray dots
+    sns.stripplot(x='number_of_hits_in_reference', y='number_of_reads', data=number_reads_mapping[number_reads_mapping['tissue_type'] == tissue_type],
+                  ax=ax, color='gray', jitter=0.2, size=5)
+
+    ax.set_title(f'Tissue Type: {tissue_type}')
+
+plt.tight_layout()
+plt.savefig('number_reads_mapping.pdf')
