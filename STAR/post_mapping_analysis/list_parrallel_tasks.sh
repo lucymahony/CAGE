aceab032db9436d3d4dac7cd245fad00173e9967
lucymahony <lucymahony7@gmail.com>
2023-09-12 15:43:40 +0100

    changed input sam file names

--- STAR/post_mapping_analysis/list_parrallel_tasks.sh
+++ STAR/post_mapping_analysis/list_parrallel_tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 declare -a tissues=("IS1" "IS2" "IS3" "SP1" "SP2" "SP3" "RO1" "RO2" "RO3" "LE1" "LE2" "LE3") 
-declare -a suffixes=("Aligned.sortedByCoord.out.bam" "Aligned.sortedByCoord.out_mapq20.bam")
+declare -a suffixes=("uniquelymapped_${tissue}.sam" "${tissue}.out.sam")
 declare -a prefixes=("UO" "UM")
 
 > tissue.list
