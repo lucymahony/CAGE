9733d6706c9ed8c1eaa6a9a94502ff49f342a8b0
Lucy Mahony <lucy.mahony7@gmail.com>
2024-04-08 14:10:10 +0200

    scripts to make alignment csv reports

--- test_snakemake/workflow/scripts/alignment_stats.py
+++ test_snakemake/workflow/scripts/alignment_stats.py
@@ -1,22 +1,28 @@
 # This file takes the output of bamtools statistics and turns it into a csv so that the results can be plotted 
 import pandas as pd
 import matplotlib.pyplot as plt 
-import sys
-
-metrics = ["Total reads", "Mapped reads", "Forward strand", "Reverse strand", "Failed QC", "Duplicates", "Paired-end reads"]
-sys.stderr = open(snakemake.log[0], "w")
-
-# Get the read-length
-values = []
-with open(snakemake.input["read_length"], 'r') as file:
-    for line in file:
-        for key in metrics:
-            if line.startswith(key):
-                value = line.split(":")[1].split()[0]
-                values.append(value)
-
-df = pd.DataFrame(values)
-df.to_csv(snakemake.output["file_path"], index=False)
 
 
+def main(snakemake):
+    list_of_metrics = ["Total reads", "Mapped reads", "Forward strand", "Reverse strand", "Failed QC", "Duplicates", "Paired-end reads"]
+    input_file_path = snakemake.input.statistics
+    dictionary = {}
+    for metric in list_of_metrics:
+        with open(input_file_path, 'r') as f:
+            for line in f:
+                if line.startswith(metric):
+
+                    result = line.split(":")[1] # Take text after : (e.g. Mapped reads:      0    (0%) becomes       0    (0%))
+                    result = result.lstrip() # (becomes 0    (0%))
+                    result = int(result.split()[0]) # Take before white space (becomes 0)
+
+                    dictionary[metric] = result
+
+    df = pd.DataFrame(dictionary, index=[0])
+    output_file_path = snakemake.output.csv
+    df.to_csv(output_file_path, index=False)
+
+
+if __name__ == "__main__":
+    main(snakemake)
 
---
+++ test_snakemake/workflow/scripts/all_alignment_stats.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def extract_variables_from_file(file_path):
+    file_name = file_path.rsplit('/', 1)[-1]
+    sample = file_name[:2]
+    repeat = file_name[2]
+    variety = file_name.split('.')[1]
+    tool = file_name.split('.')[2]
+    print(f'the sample is {sample} ,the repeat is {repeat}, the variety is {variety}, the tool is {tool}')
+    return sample, repeat, variety, tool 
+     
+
+def main(snakemake):
+    list_input_file_paths = snakemake.input
+    print(list_input_file_paths)
+    dfs = []
+    for file_path in list_input_file_paths:
+        sample, repeat, variety, tool = extract_variables_from_file(file_path)
+        df = pd.read_csv(f'../../../../../tmp/{sample}{repeat}.{variety}.{tool}.statistics.csv')
+        df['Sample'] = sample
+        df['Repeat'] = repeat
+        df['Tool'] = tool
+        dfs.append(df)
+    concatenated_df = pd.concat(dfs, ignore_index=True)
+    concatenated_df.to_csv(snakemake.output.csv, index=False)
+
+
+if __name__ == "__main__":
+    main(snakemake)
+
